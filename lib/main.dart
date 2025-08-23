3fa84b7d9f83b53f4ddaf034e8e3dbb5dd9c138e
lioytoo <arikhancis@gmail.com>
2025-08-23 15:23:36 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io'; // Required for File operations
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:opencv_dart/opencv_dart.dart' as cv;
-import 'theme/theme.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -18,8 +18,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Skeche',
       home: const GalleryAccess(),
-      theme: lightMode,
-      darkTheme: darkMode,
+      theme: ThemeData(),
+
       debugShowCheckedModeBanner: false,
     );
   }
@@ -35,14 +35,19 @@ class GalleryAccess extends StatefulWidget {
 
 
 class _GalleryAccessState extends State<GalleryAccess> {
-  File? galleryFile;        // original
-  File? sketchFile;         // processed
+  File? originalFile;       // original
+  File? workingFile;        // always overwritten
+
+  bool sketchEffect = false; // applies so the showsketch will work  
   bool showSketch = false;  // toggle between them
 
   final picker = ImagePicker();
 
   double _blurValue = 21;   // detail level (lernel size)
   double _scaleValue = 256; // intensity of division
+
+  double _brightnessValue = 0; // range: -100 to +100
+  double _contrastValue = 1.0; // range: 0.5 to 3.0
 
 
   Future getImage(ImageSource img,) async {
@@ -53,7 +58,7 @@ class _GalleryAccessState extends State<GalleryAccess> {
     setState(() {
         if (xfilePick != null) {
           // store that in global variable galleryFile in the form of File
-          galleryFile = File(pickedFile!.path);
+          originalFile = File(pickedFile!.path);
         } else {
           ScaffoldMessenger.of(context).showSnackBar(// is this context <<<
             const SnackBar(content: Text('Nothing is selected')));
@@ -92,13 +97,22 @@ class _GalleryAccessState extends State<GalleryAccess> {
     );
   }
 
-  // Here we apply the sketch
-  Future<void> sketchEffect() async {
-    if (galleryFile == null) return;
+ Future<void> applyAdjustments() async {
+  if (originalFile == null) return;
 
-    // 1. Load image into opencv
-    final src = cv.imread(galleryFile!.path);
+  // Delete previous working file if it exists
+  if (workingFile != null && await workingFile!.exists()) {
+    await workingFile!.delete();
+  }
 
+  // Always start from original
+  final src = cv.imread(originalFile!.path);
+
+  // Apply brightness + contrast
+  var proccessed = cv.convertScaleAbs(src, alpha: _contrastValue, beta: _brightnessValue);
+
+  // Optionally apply sketch
+  if (sketchEffect) {
     // 2. Convert to grayscale
     final gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY);
 
@@ -112,16 +126,17 @@ class _GalleryAccessState extends State<GalleryAccess> {
     final invertedBlur = cv.bitwiseNOT(blur);
 
     // 6. Create sketch by dividing gray by invertedBlur
-    final sketch = cv.divide(gray, invertedBlur, scale: _scaleValue);
-
-    // 7. Save result to temp file
-    final outPath = '${galleryFile!.parent.path}/sketch.png';
-    cv.imwrite(outPath, sketch);
-
-    setState(() {
-      galleryFile = File(outPath);
-      });
+    proccessed = cv.divide(gray, invertedBlur, scale: _scaleValue);
   }
+
+  // 7. Save adjusted image to file
+  final outPath = '${originalFile!.parent.path}/working.png';
+  cv.imwrite(outPath, proccessed);
+
+  setState(() {
+    workingFile = File(outPath); // Keep result in working copy so original stays as it is
+  });
+ }
 
   // Here we display Widgets + add logic to them
   @override
@@ -139,56 +154,104 @@ class _GalleryAccessState extends State<GalleryAccess> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+
+                  // First Select Image
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green,
-                      foregroundColor: Colors.white),
+                      foregroundColor: Colors.white,
+                      minimumSize: const Size(250, 40), //width: 250, height: 40
+                    ),
                     child: const Text('Select Image from Gallery or Camera'),
                     onPressed:() {
                       _pickImage(context: context);
                     },
                   ),
+
                   // Size of the selected image
                   SizedBox(
                     height: 300,
                     width: 500.0,
-                    child: galleryFile == null
+                    child: originalFile == null
                         ? const Center(child: Text('Sorry nothing selected!!',
                             style: TextStyle(color: Colors.white),
                             ),
                           )
                         : Center(
                             child: Image.file(
-                              showSketch && sketchFile != null ? sketchFile! : galleryFile!,
+                              workingFile != null ? workingFile! : originalFile!,
                             ),
                           ),
                   ),
+
+                  // Detail scroll bar - no logic yet
                   Slider(
-                    value: _blurValue,
+                    value: _blurValue,    // current slider posotion (must be double)
                     min: 1,
                     max: 51,
-                    divisions: 25,
+                    divisions: 25,        // number of steps between min and max
                     label: "Detail: ${_blurValue.toInt()}",
                     onChanged: (val) {
-                      setState(() => _blurValue.toInt);
-                      sketchEffect(); //re apply
+                      setState(() => _blurValue = val);
                     },
                   ),
+
+                  // Intensity scroll bar
                   Slider(
-                    value: _scaleValue,
+                    value: _scaleValue,             // current slider position (must be double)
                     min: 50,
                     max: 500,
-                    divisions: 45,
+                    divisions: 45,                  //number of steps between min and max
                     label: "Intensity: ${_scaleValue.toInt()}",
                     onChanged: (val) {
-                      setState(() => _scaleValue = val);
-                      sketchEffect(); //re apply
+                      setState(() => _scaleValue = val); // update the state with new value
                     },
                   ),
+
+                  
+                  
+                  // Brightness
+                  Slider(
+                    value: _brightnessValue,
+                    min: -100,
+                    max: 100,
+                    divisions: 200,
+                    label: "Brightness ${_brightnessValue.toInt()}",
+                    onChanged: (val) {
+                      setState(() => _brightnessValue = val);
+                    },
+                  ),
+
+                  // Contrast
+                  Slider(
+                    value: _contrastValue,
+                    min: 0.5,
+                    max: 3.0,
+                    divisions: 50,
+                    label: "Contrast: ${_contrastValue.toStringAsFixed(2)}",
+                    onChanged: (val) {
+                      setState(() => _contrastValue = val);
+                    },
+                  ),
+
+                    // button: click to apply all the changes
                   ElevatedButton(
-                    onPressed: sketchEffect, 
+                    onPressed: applyAdjustments,
+                    child: const Text("Apply"),
+                  ),
+
+                  // Apply the sketch effect
+                  ElevatedButton(
+                    onPressed:() {
+                      setState(() {
+                        sketchEffect = !sketchEffect;
+                        applyAdjustments;
+                      });
+                    }, 
+                    
                     child: const Text("Apply Sketch Effect"),
                   ),
+                  
                   // This button switches between the original image and the image that has the sketch effect applied
                   ElevatedButton(
                     onPressed: () {
@@ -196,7 +259,7 @@ class _GalleryAccessState extends State<GalleryAccess> {
                         showSketch = !showSketch;
                       });
                     },
-                    child: Text(showSketch ? "Show Original" : "Show Skecth"), 
+                    child: Text(showSketch ? "Show Original" : "Show Sketch"), 
                   ),
                 ],
               ),
